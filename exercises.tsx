5259a5502732b0430ef2d5d1791b0cf90461f062
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-06-26 21:05:26 -0500

    get by id

--- exercises.tsx
+++ exercises.tsx
@@ -181,9 +181,28 @@ return a[number]
 
  console.log('PREGUNTA ', randomQuestion())
 
- /**Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+ /**You are given a 0-indexed integer array costs where costs[i] is the cost of hiring the ith worker.
 
-You may assume the array is the output of JSON.parse. */
-function last() {
-    
-};
+You are also given two integers k and candidates. We want to hire exactly k workers according to the following rules:
+
+You will run k sessions and hire exactly one worker in each session.
+In each hiring session, choose the worker with the lowest cost from either the first candidates workers or the last candidates workers. Break the tie by the smallest index.
+For example, if costs = [3,2,7,7,1,2] and candidates = 2, then in the first hiring session, we will choose the 4th worker because they have the lowest cost [3,2,7,7,1,2].
+In the second hiring session, we will choose 1st worker because they have the same lowest cost as 4th worker but they have the smallest index [3,2,7,7,2]. Please note that the indexing may be changed in the process.
+If there are fewer than candidates workers remaining, choose the worker with the lowest cost among them. Break the tie by the smallest index.
+A worker can only be chosen once.
+Return the total cost to hire exactly k workers. */
+
+function totalCost(costs, k, candidates){
+  let smaller = 0;
+  let cost = 0;
+  for(let i=0; costs.length; i++){
+    if(costs[i] <= costs[i + 1]){
+      smaller = costs[i];
+    }
+    for(let i=0; k.length; i++){
+      cost = i + smaller;
+    }
+  }
+
+}
