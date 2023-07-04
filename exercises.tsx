ee1d59f0d1246ed4e74bc886cd0409d42d144d04
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-07-03 22:11:59 -0500

    exercise check if instance of

--- exercises.tsx
+++ exercises.tsx
@@ -179,32 +179,31 @@ console.log('NUMBER ', number)
 return a[number]
  }
 
- console.log('PREGUNTA ', randomQuestion())
+ //console.log('PREGUNTA ', randomQuestion())
 
- /**You are given a 0-indexed integer array costs where costs[i] is the cost of hiring the ith worker.
+ /**Write a function that checks if a given value is an instance of a given class or superclass. 
+  * For this problem, an object is considered an instance of a given class if that object has access 
+  * to that class's methods.
+There are no constraints on the data types that can be passed to the function. For example, 
+the value or the class could be undefined.*/
 
-You are also given two integers k and candidates. We want to hire exactly k workers according to the following rules:
-
-You will run k sessions and hire exactly one worker in each session.
-In each hiring session, choose the worker with the lowest cost from either the first candidates workers or the last candidates workers. Break the tie by the smallest index.
-For example, if costs = [3,2,7,7,1,2] and candidates = 2, then in the first hiring session, we will choose the 4th worker because they have the lowest cost [3,2,7,7,1,2].
-In the second hiring session, we will choose 1st worker because they have the same lowest cost as 4th worker but they have the smallest index [3,2,7,7,2]. Please note that the indexing may be changed in the process.
-If there are fewer than candidates workers remaining, choose the worker with the lowest cost among them. Break the tie by the smallest index.
-A worker can only be chosen once.
-Return the total cost to hire exactly k workers. */
-
-function totalCost(costs, k, candidates){
-  let smaller = 0;
-  let cost = 0;
-  let costsNew = [];  
-  for(let i=0; costs.length; i++){
-    if(costs[i] <= costs[i + 1]){
-      smaller = costs[i];
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(typeof obj == classFunction){
+      return true
     }
-    for(let i=0; k.length; i++){
-      cost = i + smaller;
-    }
+    return false
+};
+
+console.log(checkIfInstanceOf(new Date(), 'object'));
+
+class Rectangle {
+  alto: any;
+  ancho: any;
+  constructor(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
   }
-  console.log('smaller ', smaller);
-
 }
+const p = new Rectangle(2, 4); // ReferenceError
+
+console.log(checkIfInstanceOf(p, Rectangle));
