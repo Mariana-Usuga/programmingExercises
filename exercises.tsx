be7ed0fb4d14f04ffe4a0ae1435b3197b78464bb
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-07-04 22:48:16 -0500

    new problem

--- exercises.tsx
+++ exercises.tsx
@@ -207,3 +207,9 @@ class Rectangle {
 const p = new Rectangle(2, 4);
 
 console.log(checkIfInstanceOf(p, Rectangle));
+
+/**https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+ * Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+ */
