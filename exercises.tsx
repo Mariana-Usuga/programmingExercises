4a755a3ad949c88a566190bf3908f125f326f7d5
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-07-09 23:08:43 -0500

    new exercise

--- exercises.tsx
+++ exercises.tsx
@@ -252,3 +252,12 @@ function findMedianSortedArrays(nums1, nums2){
 }
 
 console.log('findMedianSortedArrays', findMedianSortedArrays([1,7,6,5], [2,3,4]))
+
+/**https://leetcode.com/problems/regular-expression-matching/
+ * 
+ * Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+ */
