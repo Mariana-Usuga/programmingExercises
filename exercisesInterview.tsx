3e1216ef6731dbb5e1e07a82471f8302e6e2feb2
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-07-14 12:58:21 -0500

    exercise invertircadena

--- exercisesInterview.tsx
+++ exercisesInterview.tsx
@@ -63,3 +63,37 @@ function variables(){
 
   console.log(i)// con pero si es var i=2
 }
+
+/**To find the largest number in an array. */
+
+function mayor(){
+  var arreglo = [3 , 6, 2, 56, 32, 5, 89, 32];
+var mayor = 0;
+ 
+for(i = 0; i < arreglo.length; i++){
+    if (arreglo[i] > mayor)
+    {
+        mayor = arreglo[i];
+    };
+}
+return mayor;
+}
+
+/**Reverse a string of text. */
+
+function invertirCadena(cad) {
+  // Paso 1. Usa el método split() para devolver un nuevo arreglo
+  var separarCadena = cad.split(""); // var separarCadena = "hola".split("");
+  // ["h", "o", "l", "a"]
+
+  // Paso 2. Usa el método reverse() para invertir el nuevo arreglo creado
+  var invertirArreglo = separarCadena.reverse(); // var invertirArreglo = ["h", "o", "l", "a"].reverse();
+  // ["a", "l", "o", "h"]
+
+  // Paso 3. Usa el método join() para unir todos los elementos del arreglo en una cadena
+  var unirArreglo = invertirArreglo.join(""); // var unirArreglo = ["a", "l", "o", "h"].join("");
+  // "aloh"
+  
+  //Paso 4. Devolver la cadena invertida
+  return unirArreglo; // "aloh"
+}
