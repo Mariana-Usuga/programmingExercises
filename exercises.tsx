9c5d101825d4bdd72dd258338f5fdda82f3b91b4
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-07-18 21:51:58 -0500

    Exercise new

--- exercises.tsx
+++ exercises.tsx
@@ -286,3 +286,12 @@ function isMatch(nums1, nums2){
 , and false otherwise. */
 let mar = null
 console.log('mar ', mar)
+
+/**https://leetcode.com/problems/memoize-ii/
+ * Given a function fn, return a memoized version of that function.
+
+A memoized function is a function that will never be called twice 
+with the same inputs. Instead it will return a cached value.
+
+fn can be any function and there are no constraints on what type of values it accepts. 
+Inputs are considered identical if they are === to each other. */
